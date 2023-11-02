f526cb5e26cac31f2fc1108033586b7443d5075b
Niko <nikobo2003@gmail.com>
2023-11-02 16:08:41 +0100

    add empty angular project

---
+++ SOURCE/ecad-topologie-editor/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ete-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ecad-topologie-editor';
+}
