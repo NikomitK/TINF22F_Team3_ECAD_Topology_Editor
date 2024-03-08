56595a2715004735753dcfd2626f688c8d0df371
Niko <nikobo2003@gmail.com>
2024-03-08 09:48:54 +0100

    add listItem component

---
+++ SOURCE/ecad-topologie-editor/src/app/list-item/list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ete-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-item.component.html',
+  styleUrl: './list-item.component.scss'
+})
+export class ListItemComponent {
+
+}
