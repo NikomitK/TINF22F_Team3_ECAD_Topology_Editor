ceb36dd8e40f92291735bbcbebfc622e9f111c19
Niko <nikobo2003@gmail.com>
2024-03-08 11:24:29 +0100

    install puppeteer, add npm scripts for ci

--- SOURCE/ecad-topologie-editor/src/app/app.component.spec.ts
+++ SOURCE/ecad-topologie-editor/src/app/app.component.spec.ts
@@ -19,11 +19,5 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ecad-topologie-editor');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecad-topologie-editor');
-  });
+  
 });
