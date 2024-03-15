685915e6a7e5f781c0099ef8b4cbd55bbaa8d655
Niko <nikobo2003@gmail.com>
2024-03-15 16:08:00 +0100

    refactor: header/toolbar

--- SOURCE/ecad-topologie-editor/src/app/header/header.component.ts
+++ SOURCE/ecad-topologie-editor/src/app/header/header.component.ts
@@ -1,27 +1,23 @@
-import { Component, NgModule  } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
-
-interface server{
-  value : string;
-  viewValue : string;
-}
 @Component({
   selector: 'ete-header',
   standalone: true,
-  imports: [MatToolbarModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatIconModule],
+  imports: [
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  servers: server[] = [
-    {
-      value: '',
-      viewValue: 'test',
-    }
-  ];
+  servers: string[] = ['test', 'test2', 'test3']
 }
