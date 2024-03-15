9ee3ca820d4cac63826875c1e334a576091116e9
Simon Braun <simondaniel01@gmx.de>
2024-03-15 09:54:09 +0100

    feat: added header html / ts

--- SOURCE/ecad-topologie-editor/src/app/header/header.component.ts
+++ SOURCE/ecad-topologie-editor/src/app/header/header.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, NgModule  } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
+interface server{
+  value : string;
+  viewValue : string;
+}
 @Component({
   selector: 'ete-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  test = "test hallo"
+  servers: server[] = [
+    {
+      value: '',
+      viewValue: 'test',
+    }
+  ];
 }
