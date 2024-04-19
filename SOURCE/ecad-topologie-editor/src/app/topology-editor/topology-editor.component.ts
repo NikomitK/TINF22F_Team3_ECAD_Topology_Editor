9004d36d97b89e363aa8402ae3543eae628717d3
Niko <nikobo2003@gmail.com>
2024-04-19 11:51:15 +0200

    move topology items back into class

--- SOURCE/ecad-topologie-editor/src/app/topology-editor/topology-editor.component.ts
+++ SOURCE/ecad-topologie-editor/src/app/topology-editor/topology-editor.component.ts
@@ -3,15 +3,6 @@ import { ModelListComponent } from '../model-list/model-list.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { timeout } from 'rxjs';
 
-let topologyItems = [
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Item 3' },
-    { id: 4, name: 'Item 4' },
-    { id: 5, name: 'Item 5' },
-    { id: 6, name: 'Item 6' },
-    { id: 7, name: 'Item 7' }
-];
 
 @Component({
     selector: 'ete-topology-editor',
@@ -23,6 +14,16 @@ let topologyItems = [
 
 export class TopologyEditorComponent {
 
+    topologyItems = [
+        { id: 1, name: 'Item 1' },
+        { id: 2, name: 'Item 2' },
+        { id: 3, name: 'Item 3' },
+        { id: 4, name: 'Item 4' },
+        { id: 5, name: 'Item 5' },
+        { id: 6, name: 'Item 6' },
+        { id: 7, name: 'Item 7' }
+    ];
+    
     constructor() {
 
     }
