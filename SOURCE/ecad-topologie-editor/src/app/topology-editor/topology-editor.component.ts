b48401fafb2cde9c0d9134023b92d8675beeacd8
Niko <nikobo2003@gmail.com>
2024-04-19 11:50:18 +0200

    delete test timeout in constructor

--- SOURCE/ecad-topologie-editor/src/app/topology-editor/topology-editor.component.ts
+++ SOURCE/ecad-topologie-editor/src/app/topology-editor/topology-editor.component.ts
@@ -4,29 +4,27 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { timeout } from 'rxjs';
 
 let topologyItems = [
-  { id: 1, name: 'Item 1' },
-  { id: 2, name: 'Item 2' },
-  { id: 3, name: 'Item 3' },
-  { id: 4, name: 'Item 4' },
-  { id: 5, name: 'Item 5' },
-  { id: 6, name: 'Item 6' },
-  { id: 7, name: 'Item 7' }
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+    { id: 4, name: 'Item 4' },
+    { id: 5, name: 'Item 5' },
+    { id: 6, name: 'Item 6' },
+    { id: 7, name: 'Item 7' }
 ];
 
 @Component({
-  selector: 'ete-topology-editor',
-  standalone: true,
-  imports: [ModelListComponent, DragDropModule],
-  templateUrl: './topology-editor.component.html',
-  styleUrl: './topology-editor.component.scss'
+    selector: 'ete-topology-editor',
+    standalone: true,
+    imports: [ModelListComponent, DragDropModule],
+    templateUrl: './topology-editor.component.html',
+    styleUrl: './topology-editor.component.scss'
 })
 
 export class TopologyEditorComponent {
 
-  constructor() {
-    setTimeout( function() {
-      topologyItems.push({ id: 8, name: 'Item 8' });
-    }, 50000)
-  }
+    constructor() {
+
+    }
 
 }
