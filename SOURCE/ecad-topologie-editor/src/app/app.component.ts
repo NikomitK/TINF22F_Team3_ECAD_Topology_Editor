ba9a4bed54391553c0caaf66fcc0aa40872e4094
nn6gy88 <alexander.regemann@allianz.de>
2024-05-01 17:24:48 +0200

    feat: added functionality to item search bar

--- SOURCE/ecad-topologie-editor/src/app/app.component.ts
+++ SOURCE/ecad-topologie-editor/src/app/app.component.ts
@@ -4,15 +4,23 @@ import { HeaderComponent } from './header/header.component';
 import { ModelListComponent } from './model-list/model-list.component';
 import { DetailedViewComponent } from './detailed-view/detailed-view.component';
 import { ListItemData } from './shared/list-item-data';
+import { AasServiceService } from './shared/aas-service.service';
+import { filter } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, ModelListComponent, DetailedViewComponent],
+    imports: [
+        RouterOutlet,
+        HeaderComponent,
+        ModelListComponent,
+        DetailedViewComponent
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
     title = 'ecad-topologie-editor';
 
     shownModel: ListItemData = {
@@ -23,7 +31,19 @@ export class AppComponent {
         quantity: 1
     };
 
+    itemList: ListItemData[] = [];
+
     switchShownModel(listItemData: ListItemData) {
         this.shownModel = listItemData;
     }
+    
+    filterItems(filter: string) {
+        this.itemList = this.service.getItems(filter);
+    }
+
+    constructor(private service: AasServiceService){
+        this.filterItems('');
+    }
+
+    
 }
--- SOURCE/ecad-topologie-editor/src/app/shared/aas-service.service.ts
+++ SOURCE/ecad-topologie-editor/src/app/shared/aas-service.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as demoModel from '../../assets/Submodel_Documentation.json';
+import { ListItemData } from './list-item-data';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,111 @@ export class AasServiceService {
 
   constructor(http: HttpClient) { }
 
-    getAasModel(): any {
-        return demoModel
+  getAasModel(): any {
+    return demoModel
+  }
+
+  getItems(filter: string) {
+    const items = this.items.filter(
+      (item) => [item.name,item.description].some(string => string.toLowerCase().includes(filter.toLowerCase())) 
+    ) 
+    //filtert Items deren description oder name den String der suchleiste beinhalten, nicht case sensitiv
+    return items.length? items : [] 
+  }
+
+  //temp
+  items: ListItemData [] = [
+    {
+        id: 1,
+        name: 'First Item',
+        description: 'This is the first demo item',
+        price: 10,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        name: 'Second Item',
+        description: 'This is the second demo item',
+        price: 20,
+        quantity: 2,
+    },
+    {
+        id: 3,
+        name: 'Third Item',
+        description: 'This is the third demo item',
+        price: 30,
+        quantity: 3,
+    },
+    {
+        id: 4,
+        name: 'Second Item',
+        description: 'This is the second demo item',
+        price: 20,
+        quantity: 2,
+    },
+    {
+        id: 5,
+        name: 'Third Item',
+        description: 'This is the third demo item',
+        price: 30,
+        quantity: 3,
+    },
+    {
+        id: 6,
+        name: 'Second Item',
+        description: 'This is the second demo item',
+        price: 20,
+        quantity: 2,
+    },
+    {
+        id: 7,
+        name: 'Third Item',
+        description: 'This is the third demo item',
+        price: 30,
+        quantity: 3,
+    },
+    {
+        id: 8,
+        name: 'Second Item',
+        description: 'This is the second demo item',
+        price: 20,
+        quantity: 2,
+    },
+    {
+        id: 9,
+        name: 'Third Item',
+        description: 'This is the third demo item',
+        price: 30,
+        quantity: 3,
+    },
+    {
+        id: 10,
+        name: 'Second Item',
+        description: 'This is the second demo item',
+        price: 20,
+        quantity: 2,
+    },
+    {
+        id: 11,
+        name: 'Third Item',
+        description: 'This is the third demo item',
+        price: 30,
+        quantity: 3,
+    },
+    {
+        id: 12,
+        name: 'Second Item',
+        description: 'This is the second demo item',
+        price: 20,
+        quantity: 2,
+    },
+    {
+        id: 13,
+        name: 'Third Item',
+        description: 'This is the third demo item',
+        price: 30,
+        quantity: 3,
     }
+  ]
+
 }
