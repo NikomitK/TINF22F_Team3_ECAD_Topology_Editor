ad9f91c34a082253e77138b41e1f460a918a13bc
Simon Braun <simondaniel01@gmx.de>
2024-05-14 18:21:45 +0200

    added buttons for connetions.

--- SOURCE/ecad-topologie-editor/src/app/app.component.ts
+++ SOURCE/ecad-topologie-editor/src/app/app.component.ts
@@ -9,53 +9,55 @@ import { ListItemData } from './shared/list-item-data';
 import { AasServiceService } from './shared/aas-service.service';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet, HeaderComponent, ModelListComponent, TopologyEditorComponent, DetailedViewComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ModelListComponent,
+    TopologyEditorComponent,
+    DetailedViewComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  title = 'ecad-topologie-editor';
 
-    title = 'ecad-topologie-editor';
+  editorMode = false;
 
-    editorMode = false;
-    
-    shownModel: ListItemData = {
-        id: 1,
-        name: 'First Item',
-        description: 'This is the first demo item',
-        price: 10,
-        quantity: 1
-    };
-    newItem: ListItemData = {
-        id: 1,
-        name: 'First Item',
-        description: 'This is the first demo item',
-        price: 10,
-        quantity: 1
-    };
+  shownModel: ListItemData = {
+    id: 1,
+    name: 'First Item',
+    description: 'This is the first demo item',
+    price: 10,
+    quantity: 1,
+  };
+  newItem: ListItemData = {
+    id: 1,
+    name: 'First Item',
+    description: 'This is the first demo item',
+    price: 10,
+    quantity: 1,
+  };
 
-    itemList: ListItemData[] = [];
+  itemList: ListItemData[] = [];
 
-    switchShownModel(listItemData: ListItemData) {
-        this.shownModel = listItemData;
-    }
-    addItemToEditor(listItemData: ListItemData) {
-        console.log('Adding item to editor' + listItemData.name);
-        this.topologyItems.push(listItemData);
-        this.timestamp.push(Date.now());
-    }
-    timestamp: number[] = [];
-    topologyItems: any[] = [
-    ];
+  switchShownModel(listItemData: ListItemData) {
+    this.shownModel = listItemData;
+  }
+  addItemToEditor(listItemData: ListItemData) {
+    this.topologyItems.push(listItemData);
+    this.timestamp.push(Date.now());
+  }
+  timestamp: number[] = [];
+  topologyItems: any[] = [];
 
-    filterItems(filter: string) {
-        this.itemList = this.service.getItems(filter);
-    }
+  filterItems(filter: string) {
+    this.itemList = this.service.getItems(filter);
+  }
 
-    constructor(private service: AasServiceService){
-        this.filterItems('');
-    }
-    
+  constructor(private service: AasServiceService) {
+    this.filterItems('');
+  }
 }
