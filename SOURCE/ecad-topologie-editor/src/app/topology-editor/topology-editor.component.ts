add2d0f0eb614f7d12221bd5a9efcf575fe99c16
Simon Braun <simondaniel01@gmx.de>
2024-05-14 17:26:21 +0200

    merge conflict resolved sb-dragdrop

--- SOURCE/ecad-topologie-editor/src/app/topology-editor/topology-editor.component.ts
+++ SOURCE/ecad-topologie-editor/src/app/topology-editor/topology-editor.component.ts
@@ -3,42 +3,37 @@ import { ModelListComponent } from '../model-list/model-list.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgForOf } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-    selector: 'ete-topology-editor',
-    standalone: true,
-    imports: [ModelListComponent, DragDropModule, NgForOf, MatIconModule],
-    templateUrl: './topology-editor.component.html',
-    styleUrl: './topology-editor.component.scss'
+  selector: 'ete-topology-editor',
+  standalone: true,
+  imports: [
+    ModelListComponent,
+    DragDropModule,
+    NgForOf,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  templateUrl: './topology-editor.component.html',
+  styleUrl: './topology-editor.component.scss',
 })
-
 export class TopologyEditorComponent {
-    @Input()
-    topologyItems: any[] = [];
+  @Input()
+  topologyItems: any[] = [];
 
-    @Input()
-    timestamp: number[] = [];
+  @Input()
+  timestamp: number[] = [];
 
-    constructor() {
-        
+  constructor() {}
+
+  del(event: any) {
+    console.log(event);
+    const button = event.target;
+    const id = button.id;
+    const element = document.getElementById(id);
+    if (element) {
+      element.remove();
     }
-    
-        del(event: any) {
-            console.log(event);
-            const button = event.target;
-            const id = button.id;
-            const element = document.getElementById(id);
-            if (element) {
-                element.remove();
-            }
-        }
-    
-
-    
-    
-   
+  }
 }
-
-
-
-
