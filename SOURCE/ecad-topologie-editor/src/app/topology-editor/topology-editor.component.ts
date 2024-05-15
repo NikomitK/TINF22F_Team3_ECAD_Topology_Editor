502928b688b8e59b1a72041522b1e6300b0b3cdf
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-05-15 11:08:03 +0200

    fix: test pipeline

--- SOURCE/ecad-topologie-editor/src/app/topology-editor/topology-editor.component.ts
+++ SOURCE/ecad-topologie-editor/src/app/topology-editor/topology-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ModelListComponent } from '../model-list/model-list.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgForOf } from '@angular/common';
